169c6882e8a3b84f347660eb49fdebccec867821
lebaluong33@gmail.com <lebaluong33@gmail.com>
2020-02-12 20:43:33 +0700

    Trying Redux devtool, Run Asynchronous in action, Action Creators, Utility function and Leaner switch case

--- src/containers/Counter/Counter.js
+++ src/containers/Counter/Counter.js
@@ -1,69 +1,69 @@
 import React, { Component } from 'react';
 import {connect} from 'react-redux';
 
-import * as actionTypes from '../../store/actions/actions';
+import * as actionCreators from '../../store/actions/index';
 import CounterControl from '../../components/CounterControl/CounterControl';
 import CounterOutput from '../../components/CounterOutput/CounterOutput';
 
 class Counter extends Component {
-    state = {
-        counter: 0
-    }
+	state = {
+		counter: 0
+	}
 
-    counterChangedHandler = ( action, value ) => {
-        switch ( action ) {
-            case 'inc':
-                this.setState( ( prevState ) => { return { counter: prevState.counter + 1 } } )
-                break;
-            case 'dec':
-                this.setState( ( prevState ) => { return { counter: prevState.counter - 1 } } )
-                break;
-            case 'add':
-                this.setState( ( prevState ) => { return { counter: prevState.counter + value } } )
-                break;
-            case 'sub':
-                this.setState( ( prevState ) => { return { counter: prevState.counter - value } } )
-                break;
-            default: break;
-        }
-    }
+	counterChangedHandler = ( action, value ) => {
+		switch ( action ) {
+			case 'inc':
+				this.setState( ( prevState ) => { return { counter: prevState.counter + 1 } } )
+				break;
+			case 'dec':
+				this.setState( ( prevState ) => { return { counter: prevState.counter - 1 } } )
+				break;
+			case 'add':
+				this.setState( ( prevState ) => { return { counter: prevState.counter + value } } )
+				break;
+			case 'sub':
+				this.setState( ( prevState ) => { return { counter: prevState.counter - value } } )
+				break;
+			default: break;
+		}
+	}
 
-    render () {
-        return (
-            <div>
-                <CounterOutput value={this.props.ctr} />
-                <CounterControl label="Increment" clicked={this.props.onIcrementCounter} />
-                <CounterControl label="Decrement" clicked={this.props.onDerementCounter}  />
-                <CounterControl label="Add 10" clicked={this.props.onAddCounter}  />
-                <CounterControl label="Subtract 15" clicked={this.props.onSubtractCounter}  />
-								<hr />
-								<button onClick={() => this.props.onStoreResult(this.props.ctr)}>STORE RESULTS</button>
-								<ul>
-									{this.props.storedResults.map(strResult => (
-									<li key={strResult.id} onClick={() => this.props.onDeleteResult(strResult.id)}>{strResult.value}</li>	
-									))}
-								</ul>
-            </div>
-        );
-    }
+	render () {
+		return (
+			<div>
+				<CounterOutput value={this.props.ctr} />
+				<CounterControl label="Increment" clicked={this.props.onIcrementCounter} />
+				<CounterControl label="Decrement" clicked={this.props.onDerementCounter}  />
+				<CounterControl label="Add 10" clicked={this.props.onAddCounter}  />
+				<CounterControl label="Subtract 15" clicked={this.props.onSubtractCounter}  />
+				<hr />
+				<button onClick={() => this.props.onStoreResult(this.props.ctr)}>STORE RESULTS</button>
+				<ul>
+					{this.props.storedResults.map(strResult => (
+					<li key={strResult.id} onClick={() => this.props.onDeleteResult(strResult.id)}>{strResult.value}</li>	
+					))}
+				</ul>
+			</div>
+		);
+	}
 }
 
 const mapStateToProps = state => {
-    return {
-				ctr: state.ctr.counter,
-				storedResults: state.res.results
-    }
+	return {
+		ctr: state.ctr.counter,
+		storedResults: state.res.results
+	}
 };
 
 const mapDispatchToProps = dispatch => {
-    return {
-        onIcrementCounter: () => dispatch({type: actionTypes.INCREMENT}),
-        onDerementCounter: () => dispatch({type: actionTypes.DECREMENT}),
-        onAddCounter: () => dispatch({type: actionTypes.ADD, val: 10}),
-        onSubtractCounter: () => dispatch({type: actionTypes.SUBTRACT, val: 15}),
-        onStoreResult: (result) => dispatch({type: actionTypes.STORE_RESULT, result: result}),
-        onDeleteResult: (id) => dispatch({type: actionTypes.DELETE_RESULT, resultElId: id})
-    }
+	return {
+		onIcrementCounter: () => dispatch( actionCreators.increment() ),
+		onDerementCounter: () => dispatch( actionCreators.decrement() ),
+		onAddCounter: () => dispatch( actionCreators.add(10) ),
+		onSubtractCounter: () => dispatch( actionCreators.subtract(15) ),
+		onStoreResult: (result) => dispatch( actionCreators.storeResult(result) ),
+		onDeleteResult: (id) => dispatch( actionCreators.deleteResult(id) )
+	}
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(Counter);
